7aeffeda517645ec41aa6202257d013f13fcafbf
ipmanlk <io@navinda.xyz>
2023-06-17 22:41:15 +0530

    stuff

--- generate_model.py
+++ generate_model.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix
 import pickle
 from database import get_user_data
 from pathlib import Path
+from sklearn.ensemble import RandomForestClassifier
 
 DATASET_PATH = Path(__file__).parent / "csv" / "data.csv"
 DEFAULT_MODEL_PATH = Path(__file__).parent / "models" / "model.pkl"
@@ -19,7 +19,7 @@ def generate_default_model():
   
   X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
   
-  model = LogisticRegression()
+  model = RandomForestClassifier()
   model.fit(X_train, y_train)
   
   y_pred = model.predict(X_test)
@@ -62,7 +62,7 @@ def generate_user_model(uid):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     # Create the model
-    model = LogisticRegression()
+    model = RandomForestClassifier()
     model.fit(X_train, y_train)
 
     # Save the model
